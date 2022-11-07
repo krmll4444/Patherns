92a72711e7eec7acf918883b7e516d2b9bda217a
Oleh <73743934+krmll4444@users.noreply.github.com>
2022-11-07 21:31:33 +0200

    .

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -9,80 +9,136 @@ using System.Text.Json.Serialization;
 
 namespace ConsoleApp1
 {
-    class Factory
+    public class Meeting
     {
-        class Meeting
+        public string date { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+        public string file { get; set; }
+        public Meeting(string date, string description, string url, string file)
         {
-            public string date;
-            public string description;
-            private string url;
-            public User[] users;
+            this.date = date;
+            this.description = description;
+            this.url = url;
+            this.file = file;
+        }
 
-            public Meeting(string date, string description, string url, User[] users)
+        public Meeting()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"{date, description, url, nameOfUsersFile}";
+        }
+    }
+    public class UserToFile
+    {
+        public List<User> user = new List<User>();
+        public User(){}
+        public void Add(int id, string name, string img)
+        {
+            user.Add(new User(id, name, img));
+        }
+
+        public void CreateJson(string filename)
+        {
+            string json = JsonSerializer.Serialize(user);
+
+            File.WriteAllText(filename, json);
+        }
+        public void CreateXML(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(User));
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+            using (fs)
             {
-                this.date = date;
-                this.description = description;
-                this.url = url;
-                this.users = users;
-            }
-            public void CreateJson()
-            {
-                string jsonDescription = JsonSerializer.Serialize(this.description);
-                string jsonUrl = JsonSerializer.Serialize(this.url);
-                string jsonDate = JsonSerializer.Serialize(this.date);
-                Console.WriteLine(jsonUrl);
-                Console.WriteLine(jsonDescription);
-                Console.WriteLine(jsonDate);
-            }
-            public void CreateXmlFile()
-            {
-                XmlDocument xml = new XmlDocument();
-                xml.Load("E:\\XMLFile.xml");
-                XmlElement? rootTo = xml.DocumentElement;
-                XmlElement el = xml.CreateElement("meeting");
-                XmlAttribute url = xml.CreateAttribute("url");
-                XmlElement date = xml.CreateElement("date");
-                XmlElement description = xml.CreateElement("description");
-                XmlElement user = xml.CreateElement("users");
-                XmlText urlText = xml.CreateTextNode(this.url);
-                XmlText dateText = xml.CreateTextNode(this.date);
-                XmlText descriptionText = xml.CreateTextNode(this.description);
-                url.AppendChild(urlText);
-                date.AppendChild(dateText);
-                description.AppendChild(descriptionText);
-                el.Attributes.Append(url);
-                el.AppendChild(date);
-                el.AppendChild(description);
-                rootTo?.AppendChild(el);
-                xml.Save("E:\\XMLFile.xml");
+                serializer.Serialize(fs, this);
             }
         }
-        class User
-        {
-            public int id;
-            public string name;
-            public string avatar;
+    }
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string avatar { get; set; }
 
-            public User(int id, string name, string avatar)
+        public User(int id, string name, string avatar)
+        {
+            this.id = id;
+            this.name = name;
+            this.avatar = avatar;
+        }
+        public override string ToString()
+        {
+            return $"{this.id}";
+        }
+        public User()
+        {
+
+        }
+    }
+    public class MeetingToFile
+    {
+        public List<Meeting> meet = new List<Meeting>();
+
+        public MeetingToFile()
+        {
+
+        }
+
+        public void Add(string date, string description, string url , string file)
+        {
+            meet.Add(new Meeting(date, description, url, file));
+        }
+
+        public void CreateJson(string filename)
+        {
+            string json = JsonSerializer.Serialize(onlinemeeting);
+
+            File.WriteAllText(filename, json);
+        }
+        public void CreateXML(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Meeting));
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+            using (fs)
             {
-                this.id = id;
-                this.name = name;
-                this.avatar = avatar;
-            }
-            public override string ToString()
-            {
-                return $"{this.id}";
+                serializer.Serialize(fs, this);
             }
         }
+    }
+    internal class Factory
+    { 
         static void Main(string[] args)
         {
-            User user1 = new User(1, "user1", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png");
-            User user2 = new User(2, "user2", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png");
-
-            User[] users = { user1, user2 };
-            Meeting meet = new Meeting("2022, 10, 16", "Meet", "url-link", users);
-            meet.CreateXmlFile();
-            meet.CreateJson();
+            UserToFile newUser =  new UserToFile();
+            newUser.Add(1, "Nastya", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png");
+            newUser.Add(2, "Mark", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/800px-User_icon_2.svg.png");
+            Console.WriteLine("type 'json' for .json file");
+            Console.WriteLine("type 'xml' for .xml file");
+            string input = Console.ReadLine();
+            if (input == "json")
+            {
+                MeetingToFile meetfile = new MeetingToFile();
+                meetfile.Add("02.10.2022", "some meet", "sk-sdds-dsds", "E:\\lab1\\json1.json");
+                meetfile.Add("02.10.2022", "some meet2", "sk-sdds-dsds", "E:\\lab1\\json1.json");
+                string file1 = @"E:\lab1\json1.json";
+                string file2 = @"E:\lab1\json1.json";
+                newUser.CreateJson(file2);
+                meetfile.CreateJson(file1);
+            }
+            if(input == "xml")
+            {
+                MeetingToFile meetfile = new MeetingToFile();
+                meetfile.Add("02.10.2022", "some meet", "sk-sdds-dsds", "E:\\lab1\\XMLFile1.xml");
+                meetfile.Add("02.10.2022", "some meet2", "sk-sdds-dsds", "E:\\lab1\\XMLFile1.xml");
+                string file1 = @"E:\lab1\XMLFile1.xml";
+                string file2 = @"E:\lab1\XMLFile2.xml";
+                newUser.CreateXML(file2);
+                meetfile.CreateXML(file1);
+            }
         }
     }
 }
